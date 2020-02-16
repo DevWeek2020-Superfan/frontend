cc26bfdf64d267fc3a50477e57315ff498f1d1e5
Marcus Ong <marcus.ong404@gmail.com>
2020-02-15 17:39:51 -0800

    formatting

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -53,7 +53,7 @@ const MeetingPage = () => {
   const localClient = useMemo(() => {
     const client = new RTCClient();
     if (!client._created) {
-      client.createClient({codec: stateCtx.codec, mode: stateCtx.mode});
+      client.createClient({ codec: stateCtx.codec, mode: stateCtx.mode });
       client._created = true;
     }
     return client;
@@ -73,93 +73,110 @@ const MeetingPage = () => {
       uid: 0,
       host: stateCtx.config.host,
       beauty: stateCtx.beauty
-    }
+    };
   }, [stateCtx]);
 
   const history = routerCtx.history;
 
   useEffect(() => {
     if (!config.channel) {
-      history.push('/');
+      history.push("/");
     }
   }, [config.channel, history]);
 
   useEffect(() => {
-    if (config.channel && localClient._created && localClient._joined === false) {
-      localClient.join(config).then(() => {
-        if (config.host) {
-          localClient.publish();
-        }
-        mutationCtx.stopLoading();
-      }).catch((err) => {
-        mutationCtx.toastError(`Media ${err.info}`);
-        routerCtx.history.push('/');
-      })
+    if (
+      config.channel &&
+      localClient._created &&
+      localClient._joined === false
+    ) {
+      localClient
+        .join(config)
+        .then(() => {
+          if (config.host) {
+            localClient.publish();
+          }
+          mutationCtx.stopLoading();
+        })
+        .catch(err => {
+          mutationCtx.toastError(`Media ${err.info}`);
+          routerCtx.history.push("/");
+        });
     }
   }, [localClient, mutationCtx, config, routerCtx]);
 
-  const handleClick = (name) => {
-    return (evt) => {
+  const handleClick = name => {
+    return evt => {
       evt.stopPropagation();
       switch (name) {
-        case 'video': {
-          stateCtx.muteVideo ? localStream.muteVideo() : localStream.unmuteVideo();
+        case "video": {
+          stateCtx.muteVideo
+            ? localStream.muteVideo()
+            : localStream.unmuteVideo();
           mutationCtx.setVideo(!stateCtx.muteVideo);
           break;
         }
-        case 'audio': {
-          stateCtx.muteAudio ? localStream.muteAudio() : localStream.unmuteAudio();
+        case "audio": {
+          stateCtx.muteAudio
+            ? localStream.muteAudio()
+            : localStream.unmuteAudio();
           mutationCtx.setAudio(!stateCtx.muteAudio);
           break;
         }
-        case 'screen': {
+        case "screen": {
           if (stateCtx.screen) {
-            localClient.createRTCStream({
-              token: null,
-              channel: stateCtx.config.channelName,
-              microphoneId: stateCtx.config.microphoneId,
-              resolution: stateCtx.config.resolution,
-              video: stateCtx.video,
-              audio: stateCtx.audio,
-              beauty: stateCtx.beauty,
-            }).then(() => {
-              localClient.publish();
-              mutationCtx.setScreen(false)
-            }).catch((err) => {
-              console.log(err)
-              mutationCtx.toastError(`Media ${err.info}`);
-              routerCtx.history.push('/');
-            });
+            localClient
+              .createRTCStream({
+                token: null,
+                channel: stateCtx.config.channelName,
+                microphoneId: stateCtx.config.microphoneId,
+                resolution: stateCtx.config.resolution,
+                video: stateCtx.video,
+                audio: stateCtx.audio,
+                beauty: stateCtx.beauty
+              })
+              .then(() => {
+                localClient.publish();
+                mutationCtx.setScreen(false);
+              })
+              .catch(err => {
+                console.log(err);
+                mutationCtx.toastError(`Media ${err.info}`);
+                routerCtx.history.push("/");
+              });
           } else {
-            localClient.createScreenSharingStream({
-              token: null,
-              channel: stateCtx.config.channelName,
-              microphoneId: stateCtx.config.microphoneId,
-              cameraId: stateCtx.config.cameraId,
-              resolution: stateCtx.config.resolution
-            }).then(() => {
-              localClient.publish();
-              mutationCtx.setScreen(true)
-            }).catch((err) => {
-              console.log(err)
-              mutationCtx.toastError(`Media ${err.info}`);
-              routerCtx.history.push('/');
-            });
+            localClient
+              .createScreenSharingStream({
+                token: null,
+                channel: stateCtx.config.channelName,
+                microphoneId: stateCtx.config.microphoneId,
+                cameraId: stateCtx.config.cameraId,
+                resolution: stateCtx.config.resolution
+              })
+              .then(() => {
+                localClient.publish();
+                mutationCtx.setScreen(true);
+              })
+              .catch(err => {
+                console.log(err);
+                mutationCtx.toastError(`Media ${err.info}`);
+                routerCtx.history.push("/");
+              });
           }
           break;
         }
-        case 'profile': {
+        case "profile": {
           break;
         }
         default:
           throw new Error(`Unknown click handler, name: ${name}`);
       }
-    }
-  }
+    };
+  };
 
-  const handleDoubleClick = (stream) => {
+  const handleDoubleClick = stream => {
     mutationCtx.setCurrentStream(stream);
-  }
+  };
 
   const otherStreams = useMemo(() => {
     return stateCtx.streams.filter(it => it.getId() !== currentStream.getId());
@@ -174,36 +191,65 @@ const MeetingPage = () => {
             <div className="avatar-text">Agora Test</div>
             <div className="like"></div>
           </div>
-          <div className="quit" onClick={() => {
-            localClient.leave().then(() => {
-              mutationCtx.clearAllStream();
-              routerCtx.history.push('/');
-            });
-          }}></div>
+          <div
+            className="quit"
+            onClick={() => {
+              localClient.leave().then(() => {
+                mutationCtx.clearAllStream();
+                routerCtx.history.push("/");
+              });
+            }}
+          ></div>
         </div>
-        {currentStream ?
+        {currentStream ? (
           <StreamPlayer
-            className={'main-stream-profile'}
+            className={"main-stream-profile"}
             showProfile={stateCtx.profile}
-            local={config.host ? currentStream && currentStream.getId() === localStream && localStream.getId() : false}
+            local={
+              config.host
+                ? currentStream &&
+                  currentStream.getId() === localStream &&
+                  localStream.getId()
+                : false
+            }
             stream={currentStream}
             onDoubleClick={handleDoubleClick}
             uid={currentStream.getId()}
-            domId={`stream-player-${currentStream.getId()}`}>
+            domId={`stream-player-${currentStream.getId()}`}
+          >
             <div className={classes.menuContainer}>
-              {config.host && <div className={classes.menu}>
-                  <i onClick={handleClick('video')} className={clsx(classes.customBtn, 'margin-right-19', stateCtx.muteVideo ? 'mute-video' : 'unmute-video')}/>
-                  <i onClick={handleClick('audio')} className={clsx(classes.customBtn, stateCtx.muteAudio ? 'mute-audio' : 'unmute-audio')}/>
-                {/* <i onClick={handleClick('screen')} className={clsx(classes.customBtn, stateCtx.screen ? 'start-screen-share' : 'stop-screen-share)}/> */}
-                {/* <i onClick={handleClick('profile')} className={clsx(classes.customBtn, 'show-profile')}/> */}
-              </div>}
+              {config.host && (
+                <div className={classes.menu}>
+                  <i
+                    onClick={handleClick("video")}
+                    className={clsx(
+                      classes.customBtn,
+                      "margin-right-19",
+                      stateCtx.muteVideo ? "mute-video" : "unmute-video"
+                    )}
+                  />
+                  <i
+                    onClick={handleClick("audio")}
+                    className={clsx(
+                      classes.customBtn,
+                      stateCtx.muteAudio ? "mute-audio" : "unmute-audio"
+                    )}
+                  />
+                  {/* <i onClick={handleClick('screen')} className={clsx(classes.customBtn, stateCtx.screen ? 'start-screen-share' : 'stop-screen-share)}/> */}
+                  {/* <i onClick={handleClick('profile')} className={clsx(classes.customBtn, 'show-profile')}/> */}
+                </div>
+              )}
             </div>
             <div className="stream-container">
               {otherStreams.map((stream, index) => (
                 <StreamPlayer
-                  className={'stream-profile'}
+                  className={"stream-profile"}
                   showProfile={stateCtx.profile}
-                  local={config.host ? stream.getId() === localStream && localStream.getId() : false}
+                  local={
+                    config.host
+                      ? stream.getId() === localStream && localStream.getId()
+                      : false
+                  }
                   key={index}
                   stream={stream}
                   isPlaying={stream.isPlaying()}
@@ -211,14 +257,14 @@ const MeetingPage = () => {
                   domId={`stream-player-${stream.getId()}`}
                   onDoubleClick={handleDoubleClick}
                   showUid={true}
-                >
-                </StreamPlayer>
+                ></StreamPlayer>
               ))}
             </div>
-          </StreamPlayer> : null}
+          </StreamPlayer>
+        ) : null}
       </div>
     </div>
-  )
+  );
 };
 
 export default MeetingPage;
